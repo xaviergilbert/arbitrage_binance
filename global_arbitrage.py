b149f73976ab99169aa98a76a9b824f7403d59f9
lucasdubos <lucasdubos@gmail.com>
2021-04-30 16:31:21 +0200

    optimisation et ping

--- global_arbitrage.py
+++ global_arbitrage.py
@@ -1,29 +1,38 @@
 from binance.client import Client
-
+from time import sleep, time
 
 class Arbitrage:
     def __init__(self, client, list_paires):
         self.client = client
         self.list_paires = list_paires
         self.dict_order_book = {}
-    
-    #"BAKE", "BNB", "BUSD"
-    def sell(self, wallet):
-        print("SELL")
+        self.request_nb = 0
+
+
+    # def buy_action_by_api_binance(self):
+
+    def buy_by_first_crypto(self, wallet): # if negative diff
+        print("BUY", [self.list_paires[0]])
+        self.maj_order_book()
         wallet.amount[self.list_paires[0]] = wallet.amount[self.list_paires[2]] / self.get_price(self.list_paires[0] + self.list_paires[2])
         wallet.amount[self.list_paires[2]] = 0
+        self.maj_order_book()
         wallet.amount[self.list_paires[1]] = wallet.amount[self.list_paires[0]] * self.get_price(self.list_paires[0] + self.list_paires[1])
         wallet.amount[self.list_paires[0]] = 0
+        self.maj_order_book()
         wallet.amount[self.list_paires[2]] = wallet.amount[self.list_paires[1]] * self.get_price(self.list_paires[1] + self.list_paires[2])
         wallet.amount[self.list_paires[1]] = 0
 
 
-    def buy(self, wallet):   
-        print("BUY")
+    def buy_by_second_crypto(self, wallet): # if positive diff
+        print("BUY", [self.list_paires[1]])
+        self.maj_order_book()
         wallet.amount[self.list_paires[1]] = wallet.amount[self.list_paires[2]] / self.get_price(self.list_paires[1] + self.list_paires[2])
         wallet.amount[self.list_paires[2]] = 0
+        self.maj_order_book()
         wallet.amount[self.list_paires[0]] = wallet.amount[self.list_paires[1]] / self.get_price(self.list_paires[0] + self.list_paires[1])
         wallet.amount[self.list_paires[1]] = 0
+        self.maj_order_book()
         wallet.amount[self.list_paires[2]] = wallet.amount[self.list_paires[0]] * self.get_price(self.list_paires[0] + self.list_paires[2])
         wallet.amount[self.list_paires[0]] = 0
 
@@ -38,9 +47,12 @@ class Arbitrage:
         return float(self.dict_order_book[paire]['bids'][0][1])
 
     def get_order_book(self, paire): #requete api binance
+        self.request_nb += 1
         return self.client.get_order_book(symbol=paire)
+    
+    def reinit_nb_request(self):
+        self.request_nb = 0
 
-#anciemment get_prices
     def maj_order_book(self):
         self.dict_order_book[self.list_paires[0] + self.list_paires[2]] = self.get_order_book(self.list_paires[0] + self.list_paires[2])
         self.dict_order_book[self.list_paires[1] + self.list_paires[2]] = self.get_order_book(self.list_paires[1] + self.list_paires[2])
@@ -52,3 +64,9 @@ class Arbitrage:
         return (self.prix_one_per_two - self.prix_constate) / self.prix_constate * 100
 
 
+        # tmp1 = (self.prix_one_per_two - self.prix_constate) / self.prix_constate * 100
+
+
+        # tmp2 =
+
+        # return (tmp1, tmp2)
--- main.py
+++ main.py
@@ -6,53 +6,81 @@ from global_arbitrage import Arbitrage
 
 init()
 
+def ping(client):
+    start_time = time()
+    client.ping()
+    end_time = time()
+    total_time = end_time - start_time
+    print(Fore.CYAN + "Temps de reponse de l'api binance : " + str(int(total_time * 1000)) + "ms" + Style.RESET_ALL)
+
 dict_arbitrages = {"BAKE_BNB_BUSD" : ["BAKE", "BNB", "BUSD"],
-                   "CAKE_BNB_BUSD" : ["CAKE", "BNB", "BUSD"]}
+                   "CAKE_BNB_BUSD" : ["CAKE", "BNB", "BUSD"],
+                   "DOGE_BTC_BUSD" : ["DOGE", "BTC", "BUSD"],
+                   "DOGE_BTC_USDT" : ["DOGE", "BTC", "USDT"]}
 
 class Wallet():
     def __init__(self):
-        self.amount = {'BNB' : 0, 'BUSD' : 1000, 'USDT' : 0, 'BAKE' : 0, 'TAX_INCOME' : 0}
+        self.amount = {'BNB' : 0.2, 'BUSD' : 1000, 'USDT' : 1000, 'BAKE' : 0, 'TAX_INCOME' : 0}
+
+def calcul_nb_request(list_arbitrages):
+    request_nb = 0
+    for arbitrage in list_arbitrages:
+        request_nb += arbitrage.request_nb
+        arbitrage.reinit_nb_request()
+    return request_nb
 
 def make_arbitrage(arbitrage, wallet):
-    print("ACTION ARBITRAGE")
-    print("WALLET AVANT = ", wallet.amount)
-    tmp = wallet.amount['BUSD']
-    tax = (wallet.amount['BUSD'] * (0.0750 / 100)) * 3
+    print(Fore.YELLOW + "ACTION ARBITRAGE" + Style.RESET_ALL)
+    print("WALLET AVANT = ", round(wallet.amount[arbitrage.list_paires[2]], 2))
+    tmp = wallet.amount[arbitrage.list_paires[2]]
+    tax = (wallet.amount[arbitrage.list_paires[2]] * (0.0750 / 100)) * 3
+    print("ESTIMATION WALLET AFTER ARBITRAGE =", round(wallet.amount[arbitrage.list_paires[2]] * (1 + abs(arbitrage.get_diff() / 100 )) - tax, 2))
     wallet.amount['TAX_INCOME'] += tax / 10
-    wallet.amount['BUSD'] -= tax
+    wallet.amount[arbitrage.list_paires[2]] -= tax
     print(Fore.RED + "TAX : " + str(tax) + Style.RESET_ALL)
-    print("WALLET AFTER TAX =", wallet.amount['BUSD'])
+    print("WALLET AFTER TAX =", round(wallet.amount[arbitrage.list_paires[2]], 2))
     if arbitrage.get_diff() > 0:
-        arbitrage.buy(wallet)
+        arbitrage.buy_by_second_crypto(wallet)
     else:
-        arbitrage.sell(wallet)
-    print("WALLET APRES =", wallet.amount)
-    print(Fore.GREEN + "Benefice = " + str(wallet.amount['BUSD'] - tmp) + Style.RESET_ALL)
+        arbitrage.buy_by_first_crypto(wallet)
+    print("FULL WALLET APRES =", wallet.amount)
+    print(Fore.GREEN + "Benefice = " + str(round(wallet.amount[arbitrage.list_paires[2]] - tmp), 2) + Style.RESET_ALL)
     print()
     sleep(5)
 
 if __name__ == "__main__":
+
     # recuperation info api
     mes_clefs = cle()
     api_keys = (mes_clefs.api_key, mes_clefs.api_secret)
     client = Client(api_keys[0], api_keys[1])
     wallet = Wallet()
 
+    ping(client)
+
     # init class arbitrage
     arbitrage_cake = Arbitrage(client, dict_arbitrages["CAKE_BNB_BUSD"])
     arbitrage_bake = Arbitrage(client, dict_arbitrages["BAKE_BNB_BUSD"])
 
+    list_arbitrages = []
+    for elem in dict_arbitrages:
+        list_arbitrages.append(Arbitrage(client, dict_arbitrages[elem]))
+
     while True:
-        # start_time = time()
-        arbitrage_bake.maj_order_book()
+        start_time = time()
+        for arbitrage in list_arbitrages:
+            arbitrage.maj_order_book()
+            print(" diff =", round(arbitrage.get_diff(), 4), "%", "de :", arbitrage.list_paires)
+            if arbitrage.get_diff() > 0.25:
+                make_arbitrage(arbitrage, wallet)
+        
+        end_time = time()
+        total_time = end_time - start_time
+        nb_request = calcul_nb_request(list_arbitrages)
 
-        print(" diff =", arbitrage_bake.get_diff(), "%")
+        if nb_request / total_time > 5:
+            sleep(1)
 
-        if abs(arbitrage_bake.get_diff()) > 0.10:
-            make_arbitrage(arbitrage_bake, wallet)
+        print(Fore.MAGENTA + "Time: " + str(round(total_time, 2)) + " nombres de requetes par seconde " + str(round(nb_request / total_time, 3)) + str(Style.RESET_ALL))
+        print()
 
-        sleep(0.1)
-
-        # end_time = time()
-        # total_time = end_time - start_time
-        # print("Time: ", total_time)
